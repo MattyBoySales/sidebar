ec8fd1cb7ca8fbbb2693f68e101a380012248afa
MattyBoySales <matthewrileysales1@outlook.com>
2022-11-09 08:47:39 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//.sidebar-toggle
+//.sidebar
+//close
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function() {
+//    console.log(sidebar.classList);
+//    if(sidebar.classList.contains('show-sidebar')){
+//        sidebar.classList.remove('show-sidebar')
+//    }
+//    else {
+//        sidebar.classList.add('show-sidebar');
+//    }
+sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function() {
+    sidebar.classList.remove('show-sidebar');
+})
+
